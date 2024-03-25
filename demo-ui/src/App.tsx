871b99e959c2a8b3b55e811e6e76fff9099ecee4
Diadem <d.shoukralla@topl.me>
2024-03-25 18:47:41 -0400

    Finished the frame

--- demo-ui/src/App.tsx
+++ demo-ui/src/App.tsx
@@ -8,27 +8,31 @@ import Tab from 'react-bootstrap/Tab';
 
 function App() {
   return (
-    <div className='container-fluid w-100 m-0 p-0'>
-    <Row>
-      <Col xs={3}>Demo App</Col>
-      <Col>Nav Bar</Col>
-    </Row>
-    <Tab.Container defaultActiveKey="transfer">
-      <Row className='vh-100'>
-        <Col xs={3}>
-          <Nav className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="transfer">Transfer BTC</Nav.Link>
-            </Nav.Item>
+    <div className='container-fluid w-100 m-0 p-0 bg-white'>
+      <Row className='w-100 m-0 border-bottom bg-light'>
+        <Col xs={3} className='p-3 lead logo-text m-auto'>Demo App</Col>
+        <Col className='p-3 border-start' id="topbar">
+          <Nav activeKey="btc" className="flex-row list-group">
+            <Nav.Link eventKey="bridge" className=''>Bridge</Nav.Link>
+            <Nav.Link eventKey="btc">Demo BTC Wallet</Nav.Link>
+            <Nav.Link eventKey="topl" >Demo Topl Wallet</Nav.Link>
           </Nav>
         </Col>
-        <Col>
-          <Tab.Content>
-            <Tab.Pane eventKey="transfer"><Transfer/></Tab.Pane>
-          </Tab.Content>
-        </Col>
       </Row>
-    </Tab.Container>
+      <Tab.Container defaultActiveKey="transfer">
+        <Row className='vh-100 m-0'>
+          <Col xs={3} className='p-0' id="sidebar">
+            <Nav className="flex-column">
+                <Nav.Link eventKey="transfer" className='p-3'>Transfer BTC</Nav.Link>
+            </Nav>
+          </Col>
+          <Col className='border-start'>
+            <Tab.Content>
+              <Tab.Pane eventKey="transfer"><Transfer/></Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     </div>
   )
 }
