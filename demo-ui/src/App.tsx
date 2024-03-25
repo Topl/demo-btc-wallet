4b2ff1952161477e8886472edd43840d459c4eda
Diadem <d.shoukralla@topl.me>
2024-03-25 19:54:28 -0400

    Added form.. still need to connect to wallet

--- demo-ui/src/App.tsx
+++ demo-ui/src/App.tsx
@@ -8,19 +8,19 @@ import Tab from 'react-bootstrap/Tab';
 
 function App() {
   return (
-    <div className='container-fluid w-100 m-0 p-0 bg-white'>
+    <div className='container-fluid w-100 m-0 p-0 bg-white top-0'>
       <Row className='w-100 m-0 border-bottom bg-light'>
         <Col xs={3} className='p-3 lead logo-text m-auto'>Demo App</Col>
         <Col className='p-3 border-start' id="topbar">
-          <Nav activeKey="btc" className="flex-row list-group">
-            <Nav.Link eventKey="bridge" className=''>Bridge</Nav.Link>
+          <Nav activeKey="btc">
+            <Nav.Link eventKey="bridge">Bridge</Nav.Link>
             <Nav.Link eventKey="btc">Demo BTC Wallet</Nav.Link>
             <Nav.Link eventKey="topl" >Demo Topl Wallet</Nav.Link>
           </Nav>
         </Col>
       </Row>
       <Tab.Container defaultActiveKey="transfer">
-        <Row className='vh-100 m-0'>
+        <Row className=' m-0'>
           <Col xs={3} className='p-0' id="sidebar">
             <Nav className="flex-column">
                 <Nav.Link eventKey="transfer" className='p-3'>Transfer BTC</Nav.Link>
--- demo-ui/src/views/Transfer.tsx
+++ demo-ui/src/views/Transfer.tsx
@@ -1,7 +1,37 @@
 import {FunctionComponent} from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
 const Transfer: FunctionComponent =  () => {
-  return <div>This is the transfer inner page</div>
+  return <div>
+    <h1>Transfer BTC</h1>
+    <Card>
+      <Card.Header>Transfer BTC for Peg-in</Card.Header>
+      <Card.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="recipientAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control type="text" placeholder="bcrt1q58..." />
+            <Form.Text className="text-muted">
+              The address to send BTC to.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="transferAmount">
+            <Form.Label>Quantity</Form.Label>
+            <Form.Control type="text" placeholder="1234" />
+            <Form.Text className="text-muted">
+              The amount of BTC to send to the recipient in Satoshis. 1 BTC = 100,000,000 Satoshis.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Send Funds
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+      </div>
 }
 
 export default Transfer
