a99e6b8558d93c4905492d8a5efe110ad9f5cd1f
Diadem <d.shoukralla@topl.me>
2024-03-26 15:55:26 -0400

    Fixed layout with tailwindcss

--- demo-ui/src/App.tsx
+++ demo-ui/src/App.tsx
@@ -2,45 +2,36 @@ import './App.css'
 import Transfer from './views/Transfer';
 import LoadWallet from './views/LoadWallet';
 import CreateWallet from './views/CreateWallet';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
-import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 
 
 function App() {
   return (
-    <Container className='w-100 m-0 p-0 bg-white top-0 h-100 vh-100' fluid>
-      <Row className='w-100 m-0 border-bottom bg-light'>
-        <Col xs={2} className='p-3 lead logo-text m-auto'>Demo App</Col>
-        <Col className='p-3 border-start' id="topbar">
-          <Nav activeKey="btc">
-            <Nav.Link eventKey="bridge">Bridge</Nav.Link>
-            <Nav.Link eventKey="btc">Demo BTC Wallet</Nav.Link>
-            <Nav.Link eventKey="topl" >Demo Topl Wallet</Nav.Link>
+    <div className='w-100 flex flex-col'>
+      <div className='flex bg-slate-100 border-b'>
+        <div className='select-none basis-1/6 p-3 py-4 text-2xl font-thin justify-end'>Demo App</div>
+        <Nav activeKey="btc" defaultActiveKey="btc" className='basis-11/12 items-end'>
+          <Nav.Link eventKey="bridge" className='select-none p-4 !text-slate-700 [&.active]:font-bold [&.active]:pointer-events-none'>Bridge</Nav.Link>
+          <Nav.Link eventKey="btc" className='select-none p-4 !text-slate-700 [&.active]:font-bold [&.active]:pointer-events-none'>Demo BTC Wallet</Nav.Link>
+          <Nav.Link eventKey="topl" className='select-none p-4 !text-slate-700 [&.active]:font-bold [&.active]:pointer-events-none'>Demo Topl Wallet</Nav.Link>
+        </Nav>
+      </div>
+      <div className='basis-full flex bg-white'>
+        <Tab.Container defaultActiveKey="transfer">
+          <Nav className="basis-1/6 flex-column border-r">
+            <Nav.Link eventKey="create" className='select-none p-3 hover:bg-slate-200 !text-slate-700 [&.active]:bg-slate-300 [&.active]:font-semibold [&.active]:pointer-events-none'>Create Wallet</Nav.Link>
+            <Nav.Link eventKey="load" className='select-none p-3 hover:bg-slate-200 !text-slate-700 [&.active]:bg-slate-300 [&.active]:font-semibold [&.active]:pointer-events-none'>Load Wallet</Nav.Link>
+            <Nav.Link eventKey="transfer" className='select-none p-3 hover:bg-slate-100 !text-slate-700 [&.active]:bg-slate-300 [&.active]:font-semibold [&.active]:pointer-events-none'>Transfer BTC</Nav.Link>
           </Nav>
-        </Col>
-      </Row>
-      <Tab.Container defaultActiveKey="transfer">
-        <Row className='m-0'>
-          <Col xs={2} className='p-0' id="sidebar">
-            <Nav className="flex-column">
-                <Nav.Link eventKey="create" className='p-3'>Create Wallet</Nav.Link>
-                <Nav.Link eventKey="load" className='p-3'>Load Wallet</Nav.Link>
-                <Nav.Link eventKey="transfer" className='p-3'>Transfer BTC</Nav.Link>
-            </Nav>
-          </Col>
-          <Col className='border-start'>
-            <Tab.Content>
-              <Tab.Pane eventKey="create"><CreateWallet/></Tab.Pane>
-              <Tab.Pane eventKey="load"><LoadWallet/></Tab.Pane>
-              <Tab.Pane eventKey="transfer"><Transfer/></Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
-    </Container>
+          <Tab.Content className='basis-11/12 '>
+            <Tab.Pane eventKey="create"><CreateWallet/></Tab.Pane>
+            <Tab.Pane eventKey="load"><LoadWallet/></Tab.Pane>
+            <Tab.Pane eventKey="transfer"><Transfer/></Tab.Pane>
+          </Tab.Content>
+        </Tab.Container>
+      </div>
+    </div>
   )
 }
 
